fbaea2196101e45e29fe16fc099a679db096c7c6
kushagra <kushagratripathi7@gmail.com>
2022-11-28 03:14:38 +0530

    add: added blogs

--- src/components/article/Article.tsx
+++ src/components/article/Article.tsx
@@ -1,7 +1,26 @@
 import "./article.css";
 
-const Article = () => {
-  return <h1>Article</h1>;
+type props = {
+  imgUrl: string;
+  date: string;
+  text: string;
+};
+
+const Article = ({ imgUrl, date, text }: props) => {
+  return (
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="blog_image" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{text}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
 };
 
 export default Article;
